adee208aed77e49f57357e443c99a5b8060735e8
Gaozhiwei <gzw736118298@qq.com>
2021-02-19 00:17:48 +0800

    V0.0.1 初步①

--- src/main/java/com/heihei/daily/DailyApplication.java
+++ src/main/java/com/heihei/daily/DailyApplication.java
@@ -2,8 +2,10 @@ package com.heihei.daily;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = MongoAutoConfiguration.class)
 public class DailyApplication {
 
 	public static void main(String[] args) {
